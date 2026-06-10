d62672d308a949bef1bc3e709dc6629ee9671302
AoAFH <160742595+Aoaoaooaooo@users.noreply.github.com>
2026-06-10 15:54:21 +0200

    nouveau kvikant plongeur apparement ct trop op 😂
    kvikant plongeur soumis aux conditions de tel aviv

--- screens/board/board_screen.gd
+++ screens/board/board_screen.gd
@@ -3405,7 +3405,11 @@ func pressedAllied(row):
 		if get_data(row).get_text("name") in poissons_sans_passif:
 			effect_card.data().set_text("name","card_picture_patrik_plongeur")
 			if not effect_card.data().get_text("name") == "73 - Patrik Kvikant":
+<<<<<<< Updated upstream
 				card_placed(effect_card, effect_base, true)
+=======
+				effectCard(effect_card, 0, 1000)
+>>>>>>> Stashed changes
 		else:
 			effect_card.data().set_text("name", get_data(row).get_text("name"))
 			if not effect_card.data().get_text("name") == "73 - Patrik Kvikant":
